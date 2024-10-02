dea64accedac5b6e7cf98d793e0a9ba53198172a
Chao Ming Huang <mingqed@gmail.com>
2024-10-02 13:28:07 +0800

    Implement 'RequestType'
    # Conflicts:
    #	Pokedéx.xcodeproj/project.pbxproj

---
+++ Pokedéx/RequestType.swift
@@ -0,0 +1,66 @@
+//
+//  RequestType.swift
+//  Pokedéx
+//
+//  Created by 黃昭銘 on 2024/10/2.
+//
+
+import Foundation
+
+enum HTTPMethod: String {
+    case GET = "GET"
+    case POST = "POST"
+}
+
+class RequestType {
+    let httpMethod: HTTPMethod
+    let domainURL: URL
+    let path: String
+    let queryItem: [URLQueryItem]
+    let body: Data?
+    let headers: [String: String]?
+    
+    init(httpMethod: HTTPMethod, domainURL: URL, path: String, queryItem: [URLQueryItem], body: Data? = nil, headers: [String : String]? = nil) {
+        self.httpMethod = httpMethod
+        self.domainURL = domainURL
+        self.path = path
+        self.queryItem = queryItem
+        self.body = body
+        self.headers = headers
+    }
+}
+
+// MARK: - Helpers
+private
+extension RequestType {
+    func getFullURL() -> URL {
+        var urlComponents = URLComponents(url: domainURL, resolvingAgainstBaseURL: false)
+        urlComponents?.path += path
+        if !queryItem.isEmpty {
+            urlComponents?.queryItems = queryItem
+        }
+        guard let url = urlComponents?.url else {
+            fatalError("URL should not be nil")
+            // TODO: Error handling
+        }
+        return url
+    }
+}
+
+// MARK: - Internal Methods
+extension RequestType {
+    func getRequest() -> URLRequest {
+        let fullURL = getFullURL()
+        var request = URLRequest(url: fullURL)
+        request.httpMethod = httpMethod.rawValue
+        if let body = body {
+            request.httpBody = body
+        }
+        if let headers = headers {
+            for (headerField, headerValue) in headers {
+                request.setValue(headerValue, forHTTPHeaderField: headerField)
+            }
+        }
+        return request
+    }
+}
