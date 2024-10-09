6ae7f7203c06cd16c64791dba3c11c2d289776cf
Chao Ming Huang <mingqed@gmail.com>
2024-10-09 15:26:37 +0800

    Implement 'AllPokemonListCellModel'

---
+++ Pokedéx/AllPokemonListPage/AllPokemonListCellModel.swift
@@ -0,0 +1,14 @@
+//
+//  AllPokemonListCellModel.swift
+//  Pokedéx
+//
+//  Created by 黃昭銘 on 2024/10/9.
+//
+
+import Foundation
+
+struct AllPokemonListCellModel {
+    let id: Int
+    let name: String
+    var imageData: Data?
+}
